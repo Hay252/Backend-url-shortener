dff17622e5e9b75fe7a3427785cfe3dad7f2582e
Hay252 <hayden.dodd2330@clairescourt.net>
2025-05-28 18:06:43 +0000

    feat/add input url to post route

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
+const dns = require("dns");
+const urlParser = require("url");
+
+let urls = [];
+
+app.use(bodyParser.urlencoded({extended: false}));
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -19,6 +24,11 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
+
+app.post("/api/shorturl", function(req, res) {
+  const inputUrl = req.body.url
+  
+})
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
